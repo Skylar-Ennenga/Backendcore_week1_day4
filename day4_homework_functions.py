9c0b1ad03ea9e5ce6807f07ad4c97bcdf0752afb
Skylar.Ennenga <Skylar.Ennenga@gmail.com>
2024-06-13 15:35:42 -0600

    Calculator Done

--- day4_homework_functions.py
+++ day4_homework_functions.py
@@ -8,9 +8,59 @@
 # if operation == "add":
 #     add(num_1, num_2)
 
+def addition(num1, num2):
+    add_output = (num1 + num2)
+    print(f"That eqauls {add_output}")
+
+def subtraction(num1, num2):
+    sub_output = (num1 - num2)
+    print(f"That eqauls {sub_output}")
+    
+def multiplication(num1, num2):
+    mult_output = (num1 * num2)
+    print(f"That eqauls {mult_output}")
+    
+
+def division(num1, num2):
+    div_output = (num1 / num2)
+    print(f"That eqauls {div_output}")
+    
+def main():
+    print("""
+    Please choose from the list below:
+          1. Add
+          2. Subtract
+          3. Multiply
+          4. Divide
+        """)
+    while True:
+        response = input("What would you like to do? ")
+        # conditionals to check the user response and call the necessary functions
+        if response == "1":
+            num1 = int(input("Tell me the first number you would like to use: "))
+            num2 = int(input("Tell me the second number you would like to use: "))
+            addition(num1, num2)
+            break
+        elif response == "2":
+            num1 = int(input("Tell me the first number you would like to use: "))
+            num2 = int(input("Tell me the second number you would like to use: "))
+            subtraction(num1, num2)
+            break
+        elif response == "3":
+            num1 = int(input("Tell me the first number you would like to use: "))
+            num2 = int(input("Tell me the second number you would like to use: "))
+            multiplication(num1, num2)
+            break
+        elif response == "4":
+            num1 = int(input("Tell me the first number you would like to use: "))
+            num2 = int(input("Tell me the second number you would like to use: "))
+            division(num1, num2)
+            break
+        else: 
+            print("Please choose a valid option 1, 2, 3, or 4")
 
 
-
+main()
 
 
 
