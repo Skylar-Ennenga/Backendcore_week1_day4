58dbce12ecf877e3fd1afc51a7919865b34c6233
Skylar.Ennenga <Skylar.Ennenga@gmail.com>
2024-06-13 16:11:24 -0600

    Shopping List Done

--- day4_homework_functions.py
+++ day4_homework_functions.py
@@ -9,8 +9,8 @@
 #     add(num_1, num_2)
 
 def addition(num1, num2):
-    add_output = (num1 + num2)
-    print(f"That eqauls {add_output}")
+    add_output = (num1 + num2) #adds the 2 numbers together
+    print(f"That eqauls {add_output}") # Outputs the total
 
 def subtraction(num1, num2):
     sub_output = (num1 - num2)
@@ -39,28 +39,28 @@ def main():
         if response == "1":
             num1 = int(input("Tell me the first number you would like to use: "))
             num2 = int(input("Tell me the second number you would like to use: "))
-            addition(num1, num2)
-            break
+            addition(num1, num2) # Sends the inputs to the addition function.
+            break # Only set it up to do it once.
         elif response == "2":
             num1 = int(input("Tell me the first number you would like to use: "))
             num2 = int(input("Tell me the second number you would like to use: "))
-            subtraction(num1, num2)
+            subtraction(num1, num2)# Sends the inputs to the subtraction function.
             break
         elif response == "3":
             num1 = int(input("Tell me the first number you would like to use: "))
             num2 = int(input("Tell me the second number you would like to use: "))
-            multiplication(num1, num2)
+            multiplication(num1, num2)# Sends the inputs to the multiplication function.
             break
         elif response == "4":
             num1 = int(input("Tell me the first number you would like to use: "))
             num2 = int(input("Tell me the second number you would like to use: "))
-            division(num1, num2)
-            break
+            division(num1, num2)# Sends the inputs to the division function.
+            break 
         else: 
-            print("Please choose a valid option 1, 2, 3, or 4")
+            print("Please choose a valid option 1, 2, 3, or 4") #If it doesnt get a 1-4 it askls again. 
 
 
-main()
+#main()
 
 
 
@@ -72,3 +72,42 @@ main()
 # Task 1: Write a function that lets the user add items to a list, make sure you ask the user what they would like to add (reminder: ensure the function has a parameter to receive the list).
 # Task 2: Include a feature to remove items from the list. 
 # Task 3: Add a function that prints out the entire list.
+
+
+shopping_list = []
+
+def add_item(shopping_list):
+    item = input("What would you like to add to your shopping list? ")
+    shopping_list.append(item)
+    print(shopping_list)
+
+def remove_item(shopping_list):
+    item = input("Which item would you like to remove? ")
+    if item in shopping_list:
+        shopping_list.remove(item)
+    else:
+        print(f"{item} is not in your shopping list...")
+    print(shopping_list)
+
+def main(shopping_list):
+    print("""
+    Please choose from the list below:
+          1. Add an item to your shopping list
+          2. Remove an item from your shopping list.
+          3. All done, let me see my list.
+        """)
+    while True:
+        response = input("What would you like to do? ")
+        # conditionals to check the user response and call the necessary functions
+        if response == "1":
+            add_item(shopping_list)
+        elif response == "2":
+            remove_item(shopping_list)
+        elif response == "3":
+            clean_shopping_list = ", ".join(shopping_list)
+            print(f"You will need to get {clean_shopping_list}.")
+            break
+        else: 
+            print("Please choose a valid option 1, 2, or 3")
+
+main(shopping_list)
